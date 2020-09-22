2dbe6cc2be2572b4a175ef1a60a91e0f7c0dbcce
Etienne Bourganel <etienne.bourganel.com>
2020-09-22 18:29:37 +0200

    Create basic structure for project

---
+++ script.rb
@@ -0,0 +1,14 @@
+# Create operation methods
+
+# Create random number generation methods
+
+# Create a round counting method
+
+# Create difficulty levels
+
+# Get input from user on difficulty levels
+
+# Create a time counting method
+
+
+
