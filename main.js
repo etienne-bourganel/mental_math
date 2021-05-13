8a0346927d61d8954c18d09e84e3110ecfb0148b
Etienne Bourganel <etienne.bourganel@gmail.com>
2021-05-13 19:34:50 +0200

    Add better styling for mobiles

--- main.js
+++ main.js
@@ -5,21 +5,13 @@ import { myTimer, timer, timerOff } from "./timer.js"
 // Declaration and initialization of main variables
 let timeLeft = 4
 
-// Declaration of DOM elements
-const startButton = document.querySelector("#enter")
-
 // Prepare the timer
 timerOff()
 timer.innerHTML = timeLeft.toFixed(2)
 
-// Start the timer when the Spacebar is pressed
-startButton.addEventListener("keyup", () => {
-  myTimer(timeLeft)
-})
-
 // Reads the value of the input userAnswer when the Return key is up
 document.addEventListener("keyup", (e) => {
-  if (e.key === "Spacebar") {
-    console.log("hello")
+  if (e.key === " ") {
+    myTimer(timeLeft)
   }
 })
